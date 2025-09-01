f4fe87d71b20680e91eed8cf041f30cf6f73ff81
KenJairul <jairulken@gmail.com>
2025-09-01 12:25:02 +0800

    Add link to hero-explore button

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = ({heroData, setHeroCount, heroCount, setPlayStatus, playStatus}) =>
         <p>{heroData.text2}</p>
       </div>
       <div className="hero-explore">
-      <Link to="/">Check Medical Records</Link>
+      <Link to="/search">Check Medical Records</Link>
         <img src={arrow_btn} alt=""/>
       </div>
       <div className="hero-dot-play">
